36ee1d838b7c1672f3f9900b5ce0fe99e5d7583e
Emir Hussain <emir.hussain.m@gmail.com>
2024-06-12 17:46:51 -0400

    completed 03

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,11 @@
-const reverseString = function() {
-
-};
+const reverseString = function(word) {
+    let dick = "";
+  for(let i = word.length-1;i>=0;i--){
+    dick = dick + word[i];
+  }
+  return dick;
+  };
+  
 
 // Do not edit below this line
 module.exports = reverseString;
