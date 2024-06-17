00b77474d5d74eea01f2b598a77856eca13c8aea
Emir Hussain <emir.hussain.m@gmail.com>
2024-06-17 19:10:29 -0400

    completed 5

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,24 @@
-const sumAll = function() {
+const sumAll = function(start,end) {
+    let sum = 0
+    if(start > end){
+        let holder = end
+        end = start
+        start = holder
+    }
+    else if(start < 0 ||end < 0){
+        return "ERROR"
+    }
+    else if(typeof start != "number" || typeof end != "number"){
+        return "ERROR"
 
-};
-
-// Do not edit below this line
-module.exports = sumAll;
+    }
+    while(start<=end){
+      sum+=start
+      start++
+      
+    }
+    return sum
+  };
+  
+  module.exports = sumAll;
+  
