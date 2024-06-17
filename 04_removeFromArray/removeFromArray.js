f41cce0d153485c78e1a1d14d76534089ccd72bd
Emir Hussain <emir.hussain.m@gmail.com>
2024-06-17 18:23:44 -0400

    completed 04

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,22 @@
-const removeFromArray = function() {
-};
+const removeFromArray = function(array, ...nums) {
+    
+      
+      for(let i = 0;i<nums.length;i++){
+          let count = 0;
+          let newArray = []
+          for(let j = 0;j<array.length;j++){
+              if(array[j] === nums[i]){
+  
+              }
+              else{
+                  newArray[count] = array[j];
+                  count++;
+              }
+          }
+        array = newArray
+      }
+      return array;
+  };
 
 // Do not edit below this line
 module.exports = removeFromArray;
