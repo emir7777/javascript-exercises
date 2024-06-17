e7f62a55e4fe13c0ffb27a7bf7fac1388cf19f87
Emir Hussain <emir.hussain.m@gmail.com>
2024-06-17 19:37:10 -0400

    complete 7

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,13 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(f) {
+  let c = (f - 32) * (5/9) 
+  let x = Math.round(c*10)
+  return x/10
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(c) {
+  let f = c * (9/5) + 32
+  let x = Math.round(f*10)
+  return x/10
 };
 
 // Do not edit below this line
