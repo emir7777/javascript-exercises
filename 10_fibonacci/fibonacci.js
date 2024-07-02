f0d46ba9b6eb6d57de8123ca016683a34a63b3d9
Emir Hussain <emir.hussain.m@gmail.com>
2024-07-02 18:36:09 -0400

    completed js exercise 10

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,22 @@
-const fibonacci = function() {
-
+const fibonacci = function(num) {
+    if(parseInt(num) === 0 ){
+        return 0;
+    }
+    else if(num<0){
+        return "OOPS"
+    }
+    else{
+        let arr = [];
+        for(let i = 0; i<num;i++){
+            if(i === 0 || i === 1){
+                arr[i] = 1;
+            }
+            else{
+                arr[i] = (arr[i-1] + arr[i-2])
+            }
+        }
+        return arr[num-1];
+    }
 };
 
 // Do not edit below this line
