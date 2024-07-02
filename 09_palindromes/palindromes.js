9fae7358fe0b19afed58898cc23051fdc2a70678
Emir Hussain <emir.hussain.m@gmail.com>
2024-07-02 18:19:42 -0400

    completed js exercise 9

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,16 @@
-const palindromes = function () {
-
+const palindromes = function (str) {
+    str = str.replace(/[!"#$%&'()*+,-./:;<=>?@[\]^_`{|}~ ]/g, '').toLowerCase();
+    
+    if(str.length === 0 || str.length === 1){
+        return true;
+    }
+    else{
+        let pali = "";
+        for(let i = str.length - 1;i>=0;i--){
+            pali = pali + str[i];
+        }
+        return pali === str;
+    }
 };
 
 // Do not edit below this line
