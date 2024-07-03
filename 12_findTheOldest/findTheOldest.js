3edaf54e9e75cd9cbd6e003b6e1dc8580400c988
Emir Hussain <emir.hussain.m@gmail.com>
2024-07-03 17:25:37 -0400

    completed js exercise 12

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,15 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(people) {
+    return people.reduce((person,oldest )=>{
+        const currentYear = new Date().getFullYear();
+        const personAge = (person.yearOfDeath || currentYear) - person.yearOfBirth;
+        const oldAge = (oldest.yearOfDeath || currentYear) - oldest.yearOfBirth;
+        return personAge>oldAge ? person : oldest;
+    })
 };
+
+module.exports = findTheOldest;
+
+
 
 // Do not edit below this line
 module.exports = findTheOldest;
